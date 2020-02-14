9feebff6d1a9114001d26391ec6a45e416029df3
Daniel Soares <danielsoaaraujo@hotmail.com>
2020-02-14 13:11:34 -0300

    Update testeFuncao.c

--- testeFuncao.c
+++ testeFuncao.c
@@ -2,18 +2,8 @@
  
  main(){
  
- int somar(int a, int b){
- 	
- 	return a + b;
- 	
- 	
- }
- 
- 
- 
- printf("%d",somar(2,2));
- 
- 
- 
- 
+  int somar(int a, int b){
+    return a + b;
+  }
+  printf("%d",somar(2,2));
 }
